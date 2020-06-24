9719a45c88c8f6883a03e0f79e4d307d0ecee2e1
ayukin <uh1ohdpwgm@gmail.com>
2020-06-24 19:03:35 +0900

    Initial Commit

---
+++ MyCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalendar
+//
+//  Created by 西岡鮎季 on 2020/06/24.
+//  Copyright © 2020 Ayuki Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
